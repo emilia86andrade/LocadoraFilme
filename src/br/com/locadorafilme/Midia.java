8c3ee0e384e62aa3a2b8676e11fa6ebe29272c7a
Mayara Gandolfi <112402452+MayaraGandolfi@users.noreply.github.com>
2022-10-26 11:09:27 -0300

    Criando atributos, construtores e métodos da classe Midia

--- src/br/com/locadorafilme/Midia.java
+++ src/br/com/locadorafilme/Midia.java
@@ -1,6 +1,53 @@
 package br.com.locadorafilme;
 
-public class Midia {
+public abstract class Midia {
+    private String codigo;
+    private Double preco;
+    private String nome;
+
+    public void darPlay() {
+        System.out.println("Dando o play na mídia " + getNome());
+    }
+
+    public Midia(String codigo, Double preco, String nome) {
+        this.codigo = codigo;
+        this.preco = preco;
+        this.nome = nome;
+    }
+
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        return "Midia {" +
+                "codigo='" + codigo + '\'' +
+                ", preco=" + preco +
+                ", nome='" + nome + '\'' +
+                '}';
+    }
 }
 
 
